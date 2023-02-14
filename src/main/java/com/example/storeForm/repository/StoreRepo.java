ca331edaf5ff9a88e2ae39e8d6fabe046dbccf25
Mateja Čerina <mc5620@student.uni-lj.si>
2023-02-14 16:46:56 +0100

    Update StoreRepo.java

--- src/main/java/com/example/storeForm/repository/StoreRepo.java
+++ src/main/java/com/example/storeForm/repository/StoreRepo.java
@@ -3,6 +3,6 @@ package com.example.storeForm.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.storeForm.model.Store;
 
-public interface StoreRepo extends JpaRepository<Store, Long> {
+public interface StoreRepo extends JpaRepository<Store, String> {
 
 }
